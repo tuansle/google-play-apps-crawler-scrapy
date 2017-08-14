4800eb4d89492321e29ce4366b3bfa4be3513b79
Tuan Le <tuan.le@geo.tuwien.ac.at>
2017-08-14 18:32:10 +0200

    add random time in misc

--- spiders/misc.py
+++ spiders/misc.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import csv
+import random
+import time
 
 
 def standardize_string(in_str):
@@ -143,6 +145,40 @@ def write_csv(csv_path, list_to_write):
         csvfile.close()
         spamwriter = None
 
+
+def strTimeProp(start, end, format, prop):
+    """Get a time at a proportion of a range of two formatted times.
+
+    start and end should be strings specifying times formated in the
+    given format (strftime-style), giving an interval [start, end].
+    prop specifies how a proportion of the interval to be taken after
+    start.  The returned time will be in the specified format.
+    """
+
+    stime = time.mktime(time.strptime(start, format))
+    etime = time.mktime(time.strptime(end, format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(format, time.localtime(ptime))
+
+
+def randomDate(start, end, prop):
+    '''
+    generate random time between two timeframe
+    Parameters
+    ----------
+    start
+    end
+    prop
+
+    Returns
+    -------
+
+    '''
+    return strTimeProp(start, end, '%Y-%m-%d %H:%M:%S', prop)
+
 if __name__ == "__main__":
-    csvfile = open_csv("/home/tuan/Code/google-play-apps-crawler-scrapy/csvfile/old/1.csv")
-    print csvfile
+    # csvfile = open_csv("/home/tuan/Code/google-play-apps-crawler-scrapy/csvfile/old/1.csv")
+    # print csvfile
+    print randomDate("2015-10-20 00:00:00", "2016-10-20 00:00:00", random.random())
